0662060550f6cd853dea7ceff78d6c2906b5bdfa
milmilmi <highcatch.choco@gmail.com>
2017-09-22 11:00:05 +0900

    注文確定画面から終了ボタンを押下したとき初期画面に遷移するように修正

--- SalesSystemContoroller.java
+++ SalesSystemContoroller.java
@@ -1,5 +1,6 @@
 package jp.practice.sales;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -15,12 +16,17 @@ public class SalesSystemController {
 
 	private static final String INIT = "init";
 	private static final String ADD = "add";
-	private static final String LIST = "list";
+	private static final String FIX = "fix";
+
+
+	//明細に追加する
+    private static List<Item> recordList = new ArrayList<>();
 
 
 	//文言
 	private static final String ADD_SUCCESS_MSG = "明細に追加しました";
 	private static final String ADD_ERROR_MSG = "点数には1以上の数字を入力してください。";
+	private static final String FIX_MSG = "以下のように売上登録しました。";
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,8 +40,14 @@ public class SalesSystemController {
 
 
 
-	@RequestMapping(params = "add") 
+	@RequestMapping(params = "add")
 	public String add(Model model){
+		/*
+		List<String> id = RecordManager.getItemListStr()；
+		model.addAttribute("recordList", id);
+		model.addAttribute("recordList",price);
+		model.addAllAttributes("recordList",quantity);
+		model.addAllAttributes("recordList",sum);Listに追加していきたい。。。*/
 		return ADD;
 	}
 
@@ -50,6 +62,7 @@ public class SalesSystemController {
 
 
 
+
 	/*@RequestMapping(params = "meisaiadd")
 	public String add(SalesForm form,Model model) {
 		model.addAttribute("s_message",ADD_SUCCESS_MSG);
@@ -57,6 +70,16 @@ public class SalesSystemController {
 		return ADD;
 	}*/
 
+	@RequestMapping(params = "fix")
+	public String fix(Model model){
+		return FIX;
+	}
 
+	@RequestMapping(params = "init")
+	public String syuryo(SalesForm form,Model model){
+		List<String> list = RecordManager.getItemListStr();
+		model.addAttribute("ItemList",list);
+		return INIT;
+	}
 
 }
