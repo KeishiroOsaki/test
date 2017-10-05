5923da21ff612e3e49982e5f17f6c9662706de32
milmilmi <highcatch.choco@gmail.com>
2017-10-05 19:45:41 +0900

    あげるソースミスった

--- RecordManager.java
+++ RecordManager.java
@@ -2,139 +2,92 @@ package jp.practice.sales;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import java.util.stream.Collectors;
 
 /**
- * Handles requests for the application home page.
+ * 商品データ全体を保持するクラス
  */
-@Controller
-@RequestMapping(value = "/system")
-public class SalesSystemController {
+public final class RecordManager {
 
-	private static final String INIT = "init";
-	private static final String ADD = "add";
-	private static final String FIX = "fix";
-	private static final String DELETE = "delete";
-
-
-	//明細に追加する
-    //private static List<Item> recordList = new ArrayList<Item>();
-
-	@Autowired
-	HttpSession session;
-
-	//文言
-	private static final String ADD_SUCCESS_MSG = "明細に追加しました";
-	private static final String ADD_ERROR_MSG = "点数には1以上の数字を入力してください。";
-	private static final String FIX_MSG = "以下のように売上登録しました。";
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/start")
-	public String init(SalesForm form,Model model) {
-		List<String> list = RecordManager.getItemListStr();
-		model.addAttribute("ItemList",list);
-		return INIT;
-	}
+    /** 商品データ */
+    private static List<Item> list = new ArrayList<>();
+    static {
+        list.add(new Item("A00101", "油性ボールペン", 60, 0, 0));
+        list.add(new Item("A00201", "極細ボールペン", 120, 0, 0));
+        list.add(new Item("A00301", "蛍光ペン６色セット", 420, 0, 0));
+        list.add(new Item("A00401", "シャープペンシル", 100, 0, 0));
+        list.add(new Item("A00501", "鉛筆H（１ダース）", 400, 0, 0));
+        list.add(new Item("B00101", "無線綴ノートA4", 100, 0, 0));
+        list.add(new Item("B00201", "リングノートA4", 120, 0, 0));
+        list.add(new Item("B00301", "領収書", 350, 0, 0));
+        list.add(new Item("C00101", "はさみ（青）", 128, 0, 0));
+        list.add(new Item("C00201", "ステープラー", 338, 0, 0));
+        list.add(new Item("C00301", "２穴パンチ", 128, 0, 0));
+        list.add(new Item("C00401", "ゼムクリップ", 98, 0, 0));
+        list.add(new Item("C00501", "消しゴム", 58, 0, 0));
+    }
 
 
+    private RecordManager(){
+    }
 
-	@RequestMapping(params = "add")
-	public String add(SalesForm form,Model model){
-		//List<String> meisaiList = RecordManager.setItemList();
- 		/*model.addAttribute("name",form.getName()); //商品名
-		model.addAttribute("quantity",form.getQuantity());//個数*/
+    /*　指定した名前のレコードを取得
+     *
+     * @ param name 商品名
+     * @ return Item
+     */
+  //  public static Item selectItem(String name){
 
-		/*List<String> recordList = new ArrayList<String>();*/
-		//recordList.add(form.getName(),form.getQuantity());
+//    }
 
-		//セッションからレコードリストを取り出し
-		List<Item> recordList = (List<Item>) session.getAttribute("recordList");
-
-		//Itemを格納
-		Item tmpItem = RecordManager.getItemOf(form.getName());
-
-		//getItemOfで例外処理してインスタンスにnullが入ってたら何もしないでほしいい20171005
-		if(tmpItem.getName() == null){
-
-		}else{
-		//ここまで
-
-		tmpItem.setQuantity(form.getQuantity());
-		tmpItem.setSubtotal(tmpItem.getPrice()*tmpItem.getQuantity());
-		recordList.add(tmpItem);
-
-		//レコードリストをセッションに格納
-		//session.addAttribute("recordList",recordList);
-		session.setAttribute("recordList",recordList);
+    //直接名前取ろうとおもった
+	public static String getItemListaa(){
+		String itemname = null;
+		for(int i = 0 ; i < list.size(); i++){
+			itemname = list.get(i).getName();
+			System.out.println(itemname);
 		}
-
-		return ADD;
+		return itemname;
 	}
 
+	public static List<Item> getItemList(){
+		for(int i = 0 ; i < list.size(); i++){
+			System.out.print(list.get(i));
+
+		}
+		return list;
+    }
+
+    /** 指定された名前の商品のインスタンスを新規に生成 */
+    public static Item getItemOf(String name) {
+
+    			try{
+    				Item gotItem = list.stream().filter(s -> s.getName().equals(name)).findFirst().get();
+    				return new Item(gotItem.getId(), gotItem.getName(), gotItem.getPrice(), 0, 0);
+    			}catch(Exception e){
+    				return new Item(null,null,0,0,0);
+    			}
+    			/*Item gotItem;
+    			for(int i = 0; i <= list.size(); i++){
+    				list.getName().equals(name).findFirst().get();
+    			} ＊これはforで回そうとした残骸*/
 
 
-	private void If(boolean b) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
+    }
 
 
+    /** 削除フラグがtrueのアイテムを削除 */
+    public static void deleteItem(List<Item> recordList) {
+        deleteItem = recordList.stream().filter(i -> i.getRemoveFlg() == true).findFirst().get();
+        recordList.remove(deleteItem);
 
-	@RequestMapping(params = "list")
-	public String list(Model model){
-		List<Item> list = RecordManager.getItemList();
-		model.addAttribute("ItemList",list);
-		return LIST;
-	}
+    }
 
-
-
-
-	/*@RequestMapping(params = "meisaiadd")
-	public String add(SalesForm form,Model model) {
-		model.addAttribute("s_message",ADD_SUCCESS_MSG);
-		model.addAttribute("e_message",ADD_ERROR_MSG);
-		return ADD;
-	}*/
-
-	@RequestMapping(params = "fix")
-	public String fix(Model model){
-		return FIX;
-	}
-
-	@RequestMapping(params = "delete")
-	public String delete(Model model){
-		//セッションからレコードリストを取り出し
-		List<Item> recordList = (List<Item>) session.getAttribute("recordList");
-
-		RecordManager.deleteItem(recordList);
-
-		//レコードリストをセッションに格納
-		session.setAttribute("recordList",recordList);
-
-		return DELETE;
-	}
-
-	@RequestMapping(params = "init")
-	public String syuryo(SalesForm form,Model model){
-		List<String> list = RecordManager.getItemListStr();
-		model.addAttribute("ItemList",list);
-
-		//空のレコードリストをセッションに格納
-		List<Item> recordList = new ArrayList<Item>();
-		session.setAttribute("recordList",recordList);
-
-
-		return INIT;
+	/** List<Item>をList<String>に変換するメソッド */
+	public static List<String> getItemListStr() {
+		List<String> itemNameList = list.stream().map(x -> x.getName()).collect(Collectors.toList());
+		return itemNameList;
 	}
 
 }
+//Copyright 2015 FUJITSU APPLICATIONS LIMITED
